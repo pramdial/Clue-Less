b67e7d615e5d716bac7638078a0d83608b3c0344
Peter Ramdial <37355572+pramdial@users.noreply.github.com>
2019-05-06 21:28:41 -0400

    consolidated with board

--- Hallway.py
+++
@@ -1,9 +0,0 @@
-class Hallway:
-
-
-    def __init__(self,hallWayId,occupied,player,room1,room2):
-        self.hallWayId = hallWayId
-        self.occupied = occupied
-        self.player = player
-        self.room1 = room1
-        self.room2 = room2
