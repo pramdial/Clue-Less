c32933e32042d0ac95ae1080c3b9ec9223e26b5f
Peter Ramdial <37355572+pramdial@users.noreply.github.com>
2019-05-06 21:28:26 -0400

    Consolidated with board

--- Room.py
+++
@@ -1,8 +0,0 @@
-class Room:
-
-    def __init__(self,roomName,occupied,player,weapon,secretPassage):
-        self.roomName = roomName
-        self.occupied = occupied
-        self.player = player
-        self.weapon = weapon
-        self.secretPassage = secretPassage
